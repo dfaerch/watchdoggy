fee00e3596f081c82a75e76891b87b7bfb29bbec
Dan Faerch <github.dan@hacker.dk>
2020-08-13 09:52:34 +0200

    added verbosity parameter

--- watchdoggy.pl
+++ watchdoggy.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
-# Copyright Dan Faerch, 2020.
-
+# Copyright Dan Faerch, 2020 - GPL-3.0 license - https://github.com/dfaerch/watchdoggy
 use strict;
 use warnings;
 use Getopt::Std;
@@ -12,7 +11,8 @@ my %config = (
     count    => 4,
     backoff  => 14400,
     watch    => undef,
-    action   => undef
+    action   => undef,
+    verbose  => 1
 );
 
 sub usage {
@@ -26,30 +26,31 @@ Usage:
     -c      Count - How many times watch-command must fail before running Action (default=$config{count})
     -a      Action - The command to run when something is deemed to be down.
     -b      Back-off - The number of seconds after executing Action, before it is allowed to do so again. (default=$config{backoff})
+    -v      Output versbosity. 0=silent, 1=normal output, 10=debug
 
-    Notes:
-        Count will reset after Action runs. Thus a Back-off of 0 will result in Action being run every Interval*Count seconds, as long as watch command fails.
+Examples:
+    This example runs "apachectl status" every 10 seconds.
+    If it returns non-zero exit code 5 times in a row, then we run "apachectl restart".
+    Finally, we backoff of 600 seconds (10 minutes), before it will try to do "apachectl restart" again.
 
-    Examples:
-        This example runs "apachectl status" every 10 seconds.
-        If it returns non-zero exit code 5 times in a row, then we run "apachectl restart".
-        Finally, we backoff of 600 seconds (10 minutes), before it will try to do "apachectl restart" again.
-
-        \$ ./watchdoggy.pl -i 10 -c 5 -b 600  -w "apachectl status" -a "apachectl restart"
+    \$ ./watchdoggy.pl -i 10 -c 5 -b 600  -w "apachectl status" -a "apachectl restart"
 
 
-        --
+    --
 
-        Detect if the kernel.org doesnt contain the word "Kernel" and send a mail if that happens.
-        \$ ./watchdoggy.pl -i 600 -c 3 -w "curl https://www.kernel.org | grep Kernel" -a "echo 'Kernel.org looks wrong.' | mail alert\@example.com"
+    Detect if the kernel.org doesnt contain the word "Kernel" and send a mail if that happens.
+    \$ ./watchdoggy.pl -i 600 -c 3 -w "curl https://www.kernel.org | grep Kernel" -a "echo 'Kernel.org looks wrong.' | mail alert\@example.com"
 
-        --
+    --
 
-        Wait for a local backup to complete, then shutdown machine.
-        The "find" in the example, lists files modified within the last 2 minutes, and grep matches anything. Grep will return non-zero exit code, 
-        if nothing was listed. Thus, when nothing has been modified for a while in the backup folder, we will shutdown.
+    Wait for a local backup to complete, then shutdown machine.
+    The "find" in the example, lists files modified within the last 2 minutes, and grep matches anything. Grep will return non-zero exit code, 
+    if nothing was listed. Thus, when nothing has been modified for a while in the backup folder, we will shutdown.
 
-        \$ ./watchdoggy.pl -i 5 -c 5 -w "find /home/backups/  -mmin -2 | grep . " -a "shutdown now"
+    \$ ./watchdoggy.pl -i 5 -c 5 -w "find /home/backups/  -mmin -2 | grep . " -a "shutdown now"
+
+Notes:
+    Count will reset after Action runs. Thus a Back-off of 0 will result in Action being run every Interval*Count seconds, as long as watch command fails.
 
 EOD
 ;
@@ -63,13 +64,16 @@ $config{interval} = $opts{i} if defined $opts{i};
 $config{backoff}  = $opts{b} if defined $opts{b};
 $config{count}    = $opts{c} if defined $opts{c};
 $config{action}   = $opts{a} if defined $opts{a};
+$config{verbose}  = $opts{v} if defined $opts{v};
 
 usage("Interval is invalid")            unless defined $config{interval} and $config{interval} > 0;
 usage("Count is invalid")               unless defined $config{count}    and $config{count}    > 0;
 usage("Watch-command must be defined")  unless defined $config{watch};
 usage("Action must be defined")         unless defined $config{action};
 
-sub dolog($) {
+sub dolog($$) {
+    my $loglevel = shift;
+    return unless $config{verbose} >= $loglevel; 
     print shift."\n";
 }
 
@@ -86,19 +90,20 @@ while (1) {
         if ($? != 0) {
 
             $count++;
-            dolog "Watch-command returned non-zero exit code";
+            dolog(1,"Watch-command returned non-zero exit code");
 
             if ($count >= $config{count}) {
-                dolog "Failed $count times";
+                dolog(10,"Failed $count times");
 
                 unless ($backoff) {
                     my $action_res = `$config{action}`;
-                    dolog "Running action. Output: $action_res";
+                    dolog(1,"Running action. Output: $action_res");
 
                     $count = 0;
 
                     if (defined $config{backoff} && $config{backoff}) {
                         $backoff = $config{backoff};
+                        dolog(1,"Backing off for $backoff seconds");
                     }
                 }
             }
@@ -111,7 +116,7 @@ while (1) {
     }
 
     if ($backoff) {
-        dolog "Backing off for $backoff more seconds";
+        dolog(10,"Backing off for $backoff more seconds");
         $backoff--;
         sleep 1;
     } else {
